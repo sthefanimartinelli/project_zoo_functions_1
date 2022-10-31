2d0506bba15d18f62bbadcd772c65184d147b3f3
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2022-10-31 15:42:19 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const specieType = data.species.find((specie) => specie.name === animal);
+  return specieType.residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
+
+// console.log(getAnimalsOlderThan('lions', 12));
