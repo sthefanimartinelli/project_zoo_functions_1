10f75c0a10b70d086f932c375573d11e090dc76e
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2022-10-31 15:43:49 -0300

    Requisito 2, alterei o obj destructuring do obj data

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,7 +2,7 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const specieType = data.species.find((specie) => specie.name === animal);
+  const specieType = species.find((specie) => specie.name === animal);
   return specieType.residents.every((resident) => resident.age >= age);
 }
 
