c03dd8c4f623a9897a4f2f11d75531613974d0e9
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2022-10-31 15:17:29 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  }
+  const speciesRequired = data.species.filter((specie) => {
+    if (ids.includes(specie.id)) {
+      return specie;
+    }
+  });
+  return speciesRequired;
 }
 
 module.exports = getSpeciesByIds;
+
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
