56c6d47f22f0412e994c72144cc5d0604f24908d
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2022-10-31 22:05:38 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,32 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const managers = [];
+  employees.map((employee) => employee.managers.forEach((element) => {
+    if (!managers.includes(element)) {
+      managers.push(element);
+    }
+  }));
+  return managers.includes(id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const isPersonManager = isManager(managerId);
+  if (isPersonManager === true) {
+    const subordinates = employees.reduce((acc, curr) => {
+      if (curr.managers.includes(managerId)) {
+        acc.push(`${curr.firstName} ${curr.lastName}`);
+      }
+      return acc;
+    }, []);
+    return subordinates;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
+// [ 'Burl Bethea', 'Ola Orloff', 'Emery Elser' ];
+
 module.exports = { isManager, getRelatedEmployees };
+
+// console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
