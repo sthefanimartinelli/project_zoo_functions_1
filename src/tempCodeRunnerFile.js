b5ad7c44efbef5188c0808be77bd0b306053c6ae
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2022-11-01 12:21:53 -0300

    Requisito 5 depois de apagar code runner

--- src/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-  if (!animal.sex) {
-    return species.find((specie) => specie.name === animal.specie).residents.length;
-  }
-  const filter = species.find((specie) => specie.name === animal.specie).residents
-    .filter((resident) => resident.sex === animal.sex).length;
-  return filter;
