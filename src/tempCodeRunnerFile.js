68e7a1ba9e5f987d2a66929dab93f6604c54dc20
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2022-11-01 16:21:15 -0300

    Requisito 8 sem temp code runner file

--- src/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-const entrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
