0332fe294f200885d23abbc5b5602641d3a822c3
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2022-11-01 18:11:42 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const getOldestFromFirstSpecies = (id) => {
+  const employeeRequired = employees.find((employee) => employee.id === id);
+  const firstAnimalId = employeeRequired.responsibleFor[0];
+  const animalSpecie = species.find((specie) => specie.id === firstAnimalId).residents
+    .reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
+  const returnAnimalArray = Object.values(animalSpecie);
+  return returnAnimalArray;
+};
 
 module.exports = getOldestFromFirstSpecies;
